1b318ef5eac337db32c290df1c6962fd3b9a3111
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-16 10:47:30 -0500

    validar respuesta de api

--- src/store/slices/criptoSlice.ts
+++ src/store/slices/criptoSlice.ts
@@ -1,12 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {};
+
+async function getCurrency() {
+  const url =
+    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+  const {
+    data: { Data },
+  } = await axios(url);
+  console.log(Data);
+}
 
 const currencySlice = createSlice({
   name: "currency",
   initialState,
   reducers: {},
 });
+
+getCurrency();
 
 export const {} = currencySlice.actions;
 export default currencySlice.reducer;

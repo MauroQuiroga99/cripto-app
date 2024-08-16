1d9b2baa3eb13644f6460199fe6c1509ba8e8586
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-16 08:18:51 -0500

    creando slice con  redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import CriptoSearchForm from "./componets/CriptoSearchForm";
+import { Provider } from "react-redux";
 import "./index.css";
 function App() {
   return (
     <>
-      <div className="bg-[url('/public/bg.jpg')] bg-no-repea t bg-cover h-lvh">
-        <div className="max-w-5xl w-5/12 m-auto flex flex-col text-center ">
-          <h1 className="text-white text-5xl pt-10 font-mono font-black ">
-            Cotizador de{" "}
-            <span className=" block text-teal-300">Criptomonedas</span>
-          </h1>
-          <div className=" rounded-md h-auto mt-20 px-20 py-4 bg-white shadow-2xl">
-            <CriptoSearchForm />
+      <Provider store={store}>
+        <div className="bg-[url('/public/bg.jpg')] bg-no-repea t bg-cover h-lvh">
+          <div className="max-w-5xl w-5/12 m-auto flex flex-col text-center ">
+            <h1 className="text-white text-5xl pt-10 font-mono font-black ">
+              Cotizador de{" "}
+              <span className=" block text-teal-300">Criptomonedas</span>
+            </h1>
+            <div className=" rounded-md h-auto mt-20 px-20 py-4 bg-white shadow-2xl">
+              <CriptoSearchForm />
+            </div>
           </div>
         </div>
-      </div>
+      </Provider>
     </>
   );
 }
---
+++ src/store/slices/criptoSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const currencySlice = createSlice({
+  name: "currency",
+  initialState,
+  reducers: {},
+});
+
+export const {} = currencySlice.actions;
+export default currencySlice.reducer;

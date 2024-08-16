8cbbfb879d95109c0836a6b02bbfcb3f244aa7d4
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-15 23:09:45 -0500

    crear formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import CriptoSearchForm from "./componets/CriptoSearchForm";
+import "./index.css";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-[url('/public/bg.jpg')] bg-no-repea t bg-cover h-lvh">
+        <div className="max-w-5xl w-5/12 m-auto flex flex-col text-center ">
+          <h1 className="text-white text-5xl pt-10 font-mono font-black ">
+            Cotizador de{" "}
+            <span className=" block text-teal-300">Criptomonedas</span>
+          </h1>
+          <div className=" rounded-md h-auto mt-20 px-20 py-4 bg-white shadow-2xl">
+            <CriptoSearchForm />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/componets/CriptoSearchForm.tsx
@@ -0,0 +1,39 @@
+const CriptoSearchForm = () => {
+  return (
+    <form className="flex flex-col gap-8">
+      <div className="flex flex-col gap-8">
+        <label className=" flex start text-slate-700" htmlFor="currency">
+          Moneda :
+        </label>
+        <select
+          className="bg-slate-200 p-2 rounded-md"
+          name="currency"
+          id="currency"
+        >
+          <option value="">-- Seleccione --</option>
+        </select>
+      </div>
+
+      <div className="flex flex-col gap-8">
+        <label className=" flex start text-slate-700" htmlFor="criptourrency">
+          Criptomoneda :
+        </label>
+        <select
+          className="bg-slate-200 p-2 rounded-md"
+          name="criptocurrency"
+          id="criptocurrency"
+        >
+          <option value="">-- Seleccione --</option>
+        </select>
+      </div>
+
+      <input
+        className="bg-emerald-400 p-2 rounded-md font-bold cursor-pointer uppercase hover:bg-emerald-600"
+        type="submit"
+        value="cotizar"
+      />
+    </form>
+  );
+};
+
+export default CriptoSearchForm;

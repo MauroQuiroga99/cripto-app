c5eaafb51b731e45c234ae49bedcef8b9af8f2ec
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-10-23 20:25:03 -0500

    colocar url de img completa, ajustar css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import CriptoPriceDisplay from "./componets/CriptoPriceDisplay";
-import CriptoSearchForm from "./componets/CriptoSearchForm";
+import CriptoPriceDisplay from "./components/CriptoPriceDisplay";
+import CriptoSearchForm from "./components/CriptoSearchForm";
 import "./index.css";
 
 function App() {
   return (
     <>
       <div className="min-h-screen bg-[url('/public/bg.jpg')] bg-no-repeat bg-cover flex items-center">
-        <div className="max-w-5xl w-5/12 m-auto flex flex-col text-center ">
-          <h1 className="text-white text-5xl pt-10 font-mono font-black ">
+        <div className="max-w-3xl min-w-72 w-full m-auto flex flex-col text-center justify-center ">
+          <h1 className="text-white text-4xl pt-10 font-mono font-black  ">
             Cotizador de{" "}
             <span className=" block text-teal-300">Criptomonedas</span>
           </h1>
-          <div className=" rounded-md h-auto mt-10 mb-16 px-10 py-4 bg-white shadow-2xl ">
+          <div className=" rounded-md w-4/5 h-auto mt-10 mb-16 py-8 px-10 mx-auto bg-white shadow-2xl justify-center ">
             <CriptoSearchForm />
             <CriptoPriceDisplay />
           </div>
--- src/components/CriptoPriceDisplay.tsx
+++ src/components/CriptoPriceDisplay.tsx
@@ -26,34 +26,35 @@ const CriptoPriceDisplay = () => {
   return (
     <div className=" mt-16">
       <>
-        <h2 className=" text-center font-bold">COTIZACIÓN</h2>
-        <div className="w-full grid grid-cols-[1fr_3fr] items-center pb-4 gap-14">
-          <img
-            src={`https:cryptocompare.com/${cryptoData.IMAGEURL}`}
-            alt="Imagen Cryptomoneda"
-          />
-          <div className=" flex flex-col justify-start">
-            <p className=" flex justify-start  ">
-              El Precio es de: XXXX
+        <h2 className=" text-center font-bold mb-10">COTIZACIÓN</h2>
+        <div className="w-full flex flex-wrap justify-between  items-center pb-4 gap-14">
+          <div className=" w-50 flex flex-col text-left gap-2 justify-center mx-auto">
+            <p className="  justify-start  ">
+              El Precio es de:
               <span className="font-bold"> {cryptoData.PRICE}</span>
             </p>
-            <p className=" flex justify-start ">
+            <p className="  justify-start ">
               El Precio más alto del día:
               <span className="font-bold"> {cryptoData.HIGHDAY}</span>
             </p>
-            <p className=" flex justify-start ">
+            <p className="  justify-start ">
               El Precio más bajo del día:
               <span className="font-bold"> {cryptoData.LOWDAY}</span>
             </p>
-            <p className=" flex justify-start">
+            <p className="  justify-start">
               Variación en las últimas 24 horas:
               <span className="font-bold"> {cryptoData.CHANGEPCT24HOUR}</span>
             </p>
-            <p className=" flex justify-start ">
+            <p className="  justify-start ">
               Última actualización:{" "}
               <span className="font-bold"> {cryptoData.LASTUPDATE} </span>
             </p>
           </div>
+          <img
+            src={`https://www.cryptocompare.com/${cryptoData.IMAGEURL}`}
+            alt="Imagen Cryptomoneda"
+            className="w-28 mx-auto"
+          />
         </div>
       </>
     </div>
